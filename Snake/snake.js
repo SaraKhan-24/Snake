3494042e30a84249160f871b7d2f58b43a75cff8
talatmasood <talat.masood@gmail.com>
2024-01-15 13:17:42 +0500

    2nd commit + push

--- Snake/snake.js
+++ Snake/snake.js
@@ -2,8 +2,8 @@ const gameBoard= document.querySelector("#gameBoard");
 const ctx=gameBoard.getContext("2d");
 const scoreText=document.querySelector("#scoreText");
 const resetBtn=document.querySelector("#resetBtn");
-const gameWidth=gameboard.width;
-const gameHeight=gameboard.height;
+const gameWidth=gameBoard.width;
+const gameHeight=gameBoard.height;
 const boardBackground="white";
 const snakeColor="lightgreen";
 const snakeBorder="black";
@@ -25,11 +25,54 @@ let snake=[
 window.addEventListener("keydown",changeDirection);
 resetBtn.addEventListener("click",resetGame);
 gameStart();
-function gameStart(){};
-function nextTick(){};
-function clearBoard(){};
-function createFood(){};
-function drawFood(){};
+
+function gameStart(){
+    running=true;
+    scoreText.textContent=score;
+    createFood();
+    drawFood();
+};
+function nextTick(){
+    if(running){
+        setTimeout(()=>{
+            clearBoard();
+            drawFood();
+            moveSnake();
+            drawSnake();
+            checkGameOver();
+            nextTick();
+        },75)
+    }else{
+        displayGameOver();
+    }
+};
+function clearBoard(){
+    ctx.fillStyle=boardBackground;
+    ctx.fillRect(0,0,gameWidth,gameHeight);
+};
+function createFood(){
+    function randomFood(min,max){
+        const randNum=Math.round((Math.random()*(max-min)+min)/unitSize)*unitSize;
+        return randNum;
+    }
+    foodX=randomFood(0,gameWidth-unitSize);
+    foodY=randomFood(0,gameWidth-unitSize);
+};
+function drawFood(){
+    ctx.fillStyle=foodColour;
+    ctx.fillRect(foodX,foodY,unitSize,unitSize);
+};
 function moveSnake(){};
-function drawSnake(){};
+function drawSnake(){
+    ctx.fillStyle=snakeColour;
+    ctx.strokeStyle=snakeBorder;
+    snake.forEach(snakePart=>{
+        ctx.fillRect(snakePart.x,snakePart.y,unitSize,unitSize);
+        ctx.strokeStyle(snakePart.x,snakePart.y,unitSize,unitSize);
+
+    })
+};
 function changeDirection(){};
+function checkGameOver(){};
+function displayGameOver(){};
+function resetGame(){};
