3c3ab9b78a43f3b2f9512f8782197222eda12c9e
talatmasood <talat.masood@gmail.com>
2024-01-16 23:21:24 +0500

    Particle animation

--- Snake/snake.js
+++ Snake/snake.js
@@ -1,7 +1,5 @@
 const gameBoard= document.querySelector("#gameBoard");
 const ctx=gameBoard.getContext("2d");
-const scoreText=document.querySelector("#scoreText");
-const resetBtn=document.querySelector("#resetBtn");
 const margin=40;
 const windowWidth=window.innerWidth;
 const windowHeight=window.innerHeight;
@@ -9,6 +7,17 @@ const gameWidth=Math.floor(windowWidth-margin+20);
 const gameHeight=Math.floor(windowHeight-margin+20);
 gameBoard.width=gameWidth;
 gameBoard.height=gameHeight;
+
+let particleArray;
+
+
+
+
+
+
+const scoreText=document.querySelector("#scoreText");
+const resetBtn=document.querySelector("#resetBtn");
+
 const gradient = ctx.createLinearGradient(0, 0, gameWidth, gameHeight); // Adjust as needed
 gradient.addColorStop(0, '#2f0068');
 gradient.addColorStop(1, '#000000');
@@ -27,8 +36,93 @@ let snake=[
     {x:unitSize*3, y:0},
     {x:unitSize*2, y:0},
     {x:unitSize, y:0},
-    {x:0, y:0}
+    {x:0, y:0, radius: unitSize/2}
 ];
+window.addEventListener("snakemove",
+function(event){
+    snake[0].x=event.x;
+    snake[0].y=event.y;
+}
+);
+class Particle{
+    constructor(x,y,directionX,directionY,size,color){
+        this.x=x;
+        this.y=y;
+        this.directionX=directionX;
+        this.directionY=directionY;
+        this.size=size;
+        this.color=color;
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,this.size,0,Math.PI*2,false);
+        ctx.fillStyle='#c78cf7';
+        ctx.fill();
+    }
+    update(){
+        if(this.x>gameBoard.width||this.x<0){
+            this.directionX=-this.directionX;
+        }
+        if(this.y>gameBoard.height||this.y<0){
+            this.directionY=-this.directionY;
+        }
+        let dx = this.x - snake[0].x;
+        let dy = this.y - snake[0].y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < 150) { // Adjust the distance as needed
+          this.directionX += dx / 100; // Repel from snake head
+          this.directionY += dy / 100;
+        }
+        this.x+=this.directionX/7;
+        this.y+=this.directionY/7;
+        this.draw();
+    }
+}
+function init(){
+    particleArray=[];
+    let numberofParticles=(gameBoard.height*gameBoard.width)/50000;
+    for(let i=0;i<numberofParticles;i++){
+        let size=(Math.random()*5)+1;
+        let x=(Math.random()*((gameWidth-size*2)-(size*2))+size*2);
+        let y=(Math.random()*((gameHeight-size*2)-(size*2))+size*2);
+        let directionX=(Math.random()*5)-2.5;
+        let directionY=(Math.random()*5)-2.5;
+        let color='#c78cf7';
+    
+        particleArray.push(new Particle(x,y,directionX,directionY,size,color) );
+
+    }
+}
+function connect(){
+    for(let a=0;a<particleArray.length;a++){
+        for(let b=a;b<particleArray.length;b++){
+            let distance=((particleArray[a].x-particleArray[b].x)
+            *(particleArray[a].x-particleArray[b].x))
+            +((particleArray[a].y-particleArray[b].y)
+            *(particleArray[a].y-particleArray[b].y));
+            if(distance<(gameWidth/7)*(gameHeight/7)){
+                ctx.strokeStyle='c78cf7';
+                ctx.lineWidth=1;
+                ctx.beginPath();
+                ctx.moveTo(particleArray[a].x,particleArray[a].y);
+                ctx.lineTo(particleArray[b].x,particleArray[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
+function animate(){
+    requestAnimationFrame(animate);
+    
+    ctx.clearRect(0,0,gameWidth,gameHeight);
+    drawSnake(); // Draw snake before particles
+    drawFood(); 
+
+    for(let i=0;i<particleArray.length;i++){
+        particleArray[i].update();
+    }
+    connect();
+}
 
 window.addEventListener("keydown",changeDirection);
 resetBtn.addEventListener("click",resetGame);
@@ -37,16 +131,21 @@ gameStart();
 function gameStart(){
     running=true;
     scoreText.textContent=score;
+    init();
+    animate();
     createFood();
     drawFood();
     createFood();
+
     nextTick();
+    
 };
 function nextTick(){
 
     console.log(running);
     if(running){
         setTimeout(()=>{
+            
             clearBoard();
             drawFood();
             moveSnake();
@@ -90,9 +189,11 @@ function moveSnake(){
                 y: snake[0].y+yVelocity};
     snake.unshift(head);
     //if food is eaten
-   
+    let dx=snake[0].x-foodX;
+    let dy=snake[0].y-foodY;
+    let distance=Math.sqrt(dx*dx+dy*dy);
 
-    if((snake[0].x==foodX&&snake[0].y==foodY)){
+    if((snake[0].x==foodX&&snake[0].y==foodY)||(distance<snake[0].radius+(unitSize/2))){
         score++;
         scoreText.textContent=score;
         createFood();
@@ -167,13 +268,15 @@ function checkGameOver(){
     for(let i=1;i<snake.length;i++){
         if(snake[i].x==snake[0].x&&snake[i].y==snake[0].y){
             running=false;
+            displayGameOver();
         }
     }
 };
 function displayGameOver(){
-    ctx.font="50px MV Boli";
-    ctx.textAlign="center";
-    ctx.fillText("GAME OVER",gameWidth/2,gameHeight/2);
+    ctx.font = "50px MV Boli"; // Set font for clarity
+    ctx.fillStyle = "red"; // Set text color to red
+    ctx.textAlign = "center"; // Center the text horizontally
+    ctx.fillText("GAME OVER", gameWidth / 2, gameHeight / 2); 
     running=false;
 };
 function resetGame(){
